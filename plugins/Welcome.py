b1163ac20b24c0518273f493635dd83f02919e5d
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-25 19:25:16 +0530

    Update Welcome.py

--- plugins/Welcome.py
+++ plugins/Welcome.py
@@ -1,1 +1,18 @@
 
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+
+
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} , How are you? Happy to here😌 ",chat_id=chatid)
+	
+@Client.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Sheri enna😌",chat_id=chatid)
+	
+
